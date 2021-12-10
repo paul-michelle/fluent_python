3536f414a244b2ec9ec141df6125bf942300027e
paul-michelle <paulmichellegallery@gmail.com>
2021-12-10 19:42:30 +0300

    Solve sleep problem with a separate delayed tasks container

--- david_beazley/async/concurrency_without_threads.py
+++ david_beazley/async/concurrency_without_threads.py
@@ -2,65 +2,73 @@ import collections
 import time
 from typing import Callable, Any, Tuple, Union
 
-SLEEP_TIME = .5
+SLEEP_TIME = 1
 
 
 class Scheduler:
 
     def __init__(self):
-        self._tasks = collections.deque()
+        self._tasks_ready = collections.deque()
+        self._tasks_delayed = []
 
-    def run_soon(self, funcs: Union[Callable[[...], Any], Tuple[Callable[[...], Any]]]) -> None:
-        if not isinstance(funcs, tuple):
-            funcs = (funcs,)
-        self._tasks.extend(funcs)
+    def run_soon(self, func: Callable[[...], Any], delay: Union[int, float] = 0) -> None:
+        ready = not bool(delay)
+        if delay:
+            deadline = time.time() + delay
+            self._tasks_delayed.append(
+                (deadline, func)
+            )
+            self._tasks_delayed.sort()
+        if ready:
+            self._tasks_ready.append(func)
 
     def run(self) -> None:
-        while self._tasks:
-            task = self._tasks.popleft()
-            task()
+
+        while self._tasks_ready or self._tasks_delayed:
+
+            if not self._tasks_ready:
+                deadline, task = self._tasks_delayed.pop(0)
+                time_to_await = deadline - time.time()
+                if time_to_await > 0:
+                    time.sleep(time_to_await)
+                self._tasks_ready.append(task)
+
+            while self._tasks_ready:
+                task = self._tasks_ready.popleft()
+                task()
 
 
-def count_down(start: int, scheduler: Scheduler) -> None:
+def count_down(scheduler: Scheduler, start: int = 0) -> None:
     if start >= 0:
-        print(start)
-        time.sleep(SLEEP_TIME)
-        scheduler.run_soon(lambda: count_down(start - 1, scheduler))
+        print(f'Down {start}')
+        scheduler.run_soon(
+            lambda: count_down(scheduler, start - 1),
+            delay=SLEEP_TIME * 2,
+        )
+
 
 
 def count_up(scheduler: Scheduler, start: int = 0, stop: int = 0) -> None:
 
-    def _print_n_sleep() -> None:
+    def _do() -> None:
         nonlocal start
         if start <= stop:
-            print(start)
+            print(f'Up {start}')
             start += 1
-            time.sleep(SLEEP_TIME)
-            scheduler.run_soon(lambda: _print_n_sleep())
+            scheduler.run_soon(
+                lambda: _do(),
+                delay=SLEEP_TIME
+            )
 
-    _print_n_sleep()
-
-
-def count_up_rec(scheduler: Scheduler, start: int = 0, stop: int = 0) -> None:
-    if start <= stop:
-        print(start)
-        time.sleep(SLEEP_TIME)
-        scheduler.run_soon(lambda: count_up_rec(scheduler, start + 1, stop))
+    _do()
 
 
 if __name__ == '__main__':
     schler = Scheduler()
-    schler.run_soon(
-        (lambda: count_down(5, schler), lambda: count_up(schler, stop=5))
-    )
+    schler.run_soon(lambda: count_down(schler, start=5))
+    schler.run_soon(lambda: count_up(schler, stop=5))
+
     try:
         schler.run()
     except KeyboardInterrupt:
-        print(
-            """But then again, if there were a 20-sec sleep inside one of the two
-            functions, the other function will have to wait. It is blocking.
-            So, their running in turn kind of 'giving back control' is by no means enough.
-            
-            The funny stuff, though, is that this behaviour mimics the two generators, with
-            popping a task, executing it and appending it back."""
-        )
+        pass
