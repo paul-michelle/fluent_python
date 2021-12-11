58a82ef1cd7bf6621dfa5318290873bfb4187f8c
paul-michelle <paulmichellegallery@gmail.com>
2021-12-11 11:24:07 +0300

    Solve a corner case of several deadlines being equal

--- david_beazley/async/concurrency_without_threads.py
+++ david_beazley/async/concurrency_without_threads.py
@@ -1,6 +1,7 @@
 import collections
 import time
-from typing import Callable, Any, Tuple, Union
+import heapq
+from typing import Callable, Any, Union
 
 SLEEP_TIME = 1
 
@@ -10,15 +11,17 @@ class Scheduler:
     def __init__(self):
         self._tasks_ready = collections.deque()
         self._tasks_delayed = []
+        self._sequence = 0
 
-    def run_soon(self, func: Callable[[...], Any], delay: Union[int, float] = 0) -> None:
-        ready = not bool(delay)
-        if delay:
-            deadline = time.time() + delay
-            self._tasks_delayed.append(
-                (deadline, func)
+    def run_soon(self, func: Callable[[...], Any], delayed: Union[int, float] = 0) -> None:
+        ready = not bool(delayed)
+        if delayed:
+            self._sequence += 1
+            deadline = time.time() + delayed
+            heapq.heappush(
+                self._tasks_delayed,
+                (deadline, self._sequence, func)
             )
-            self._tasks_delayed.sort()
         if ready:
             self._tasks_ready.append(func)
 
@@ -27,7 +30,7 @@ class Scheduler:
         while self._tasks_ready or self._tasks_delayed:
 
             if not self._tasks_ready:
-                deadline, task = self._tasks_delayed.pop(0)
+                deadline, _, task = heapq.heappop(self._tasks_delayed)
                 time_to_await = deadline - time.time()
                 if time_to_await > 0:
                     time.sleep(time_to_await)
@@ -43,13 +46,11 @@ def count_down(scheduler: Scheduler, start: int = 0) -> None:
         print(f'Down {start}')
         scheduler.run_soon(
             lambda: count_down(scheduler, start - 1),
-            delay=SLEEP_TIME * 2,
+            delayed=SLEEP_TIME * 10,
         )
 
 
-
 def count_up(scheduler: Scheduler, start: int = 0, stop: int = 0) -> None:
-
     def _do() -> None:
         nonlocal start
         if start <= stop:
@@ -57,7 +58,7 @@ def count_up(scheduler: Scheduler, start: int = 0, stop: int = 0) -> None:
             start += 1
             scheduler.run_soon(
                 lambda: _do(),
-                delay=SLEEP_TIME
+                delayed=SLEEP_TIME
             )
 
     _do()
@@ -65,8 +66,8 @@ def count_up(scheduler: Scheduler, start: int = 0, stop: int = 0) -> None:
 
 if __name__ == '__main__':
     schler = Scheduler()
-    schler.run_soon(lambda: count_down(schler, start=5))
-    schler.run_soon(lambda: count_up(schler, stop=5))
+    schler.run_soon(lambda: count_down(schler, start=10))
+    schler.run_soon(lambda: count_up(schler, stop=10))
 
     try:
         schler.run()
